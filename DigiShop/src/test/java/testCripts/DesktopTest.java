fc77f8795efbd658d539ed9bf3d5e4e38a692605
User <User@DESKTOP-A5CHD3A>
2024-04-22 14:05:09 +0530

    Added new steps in desktoptest_001

--- DigiShop/src/test/java/testCripts/DesktopTest.java
+++ DigiShop/src/test/java/testCripts/DesktopTest.java
@@ -1,5 +1,7 @@
 package testCripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import genericLibrary.BaseClass;
@@ -9,5 +11,7 @@ public class DesktopTest extends BaseClass {
 	@Test
 	public void desktop_001() {
 		//Git hub practice
+		
+		//driver.findElement(By.id("gvh")).click();
 	}
 }
