5b2c587658e275b1daa69052e49e567c18a0267e
User <User@DESKTOP-A5CHD3A>
2024-04-22 13:59:56 +0530

    Added dektoptest_001 test case

---
+++ DigiShop/src/test/java/testCripts/DesktopTest.java
@@ -0,0 +1,13 @@
+package testCripts;
+
+import org.testng.annotations.Test;
+
+import genericLibrary.BaseClass;
+
+public class DesktopTest extends BaseClass {
+
+	@Test
+	public void desktop_001() {
+		//Git hub practice
+	}
+}
